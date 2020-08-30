c179147221e07d84399fcf372be927dd7e6160fb
PeterEhses <ehsesp@hochschule-trier.de>
2020-08-30 20:27:58 +0200

    modal X fix, image background color added, single gallery item added image with lightbox

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,21 +1,30 @@
 <template>
 <div class="modal" @click="$emit('click')">
+  <div class="backdrop">
+
+  </div>
   <div class="modal-inner">
     <slot>something went wrong</slot>
   </div>
+  <close/>
 
 
 </div>
 </template>
 
 <script>
+import Close from 'vue-material-design-icons/Close.vue';
         export default {
-            name: 'Modal'
+            name: 'Modal',
+            components: {
+              Close
+            }
         };
     </script>
 
 <style lang="scss" scoped>
 .modal{
+  background: rgba(27, 27, 27, 0.5);
   z-index: 1200;
   //background: red;
   position: fixed;
@@ -23,16 +32,74 @@
   left: 0;
   width: 100%;
   height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
 }
 .modal-inner{
+  z-index: 1;
+  position: relative;
+  //background: red;
   width: 85%;
   height: 75%;
   margin: 10% 5% 10% 10%;
   width: 100%;
+  width: 100vw;
   height: 100%;
   margin: 0;
-  padding: 10.5vmin;
-  padding-right: 5vmin;
+  padding: calc(10.5vmin + 2vmin);
+  padding-right: calc(5vmin + 1.3vmin);
   box-sizing: border-box;
+
+}
+.backdrop{
+  z-index: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  margin: calc(10.5vmin + 2vmin);
+  margin-right: 5vmin;
+  width: calc(100% - 15.5vmin - 3.5vmin); // thanks firefox
+  height: calc(100% - 21vmin - 4vmin);
+  background: $bg-dark;
+  border: $outline-weight solid $highlight-default;
+  border-radius: 0 0 1.5*$frame-constant 0;
+}
+.material-design-icon{
+  z-index: 9001;
+  width: 2em;
+  height: 2em;
+  background: $highlight-default;
+  border: $outline-weight solid $white;
+  border-radius: 12345px;
+  position: fixed;
+  top: calc(10.5vmin - 1em + 1.9vmin);
+  right: calc(5vmin - 1em + 1.2vmin);
+  svg{
+    width: 2em;
+    height: 2em;
+  }
+}
+@media (max-width: 800px){
+  .modal-inner{
+    padding: calc(10.5vmin + 4vmin);
+    padding-right: calc(5vmin + 5.5vmin);
+  }
+  .backdrop{
+    margin: calc(10.5vmin + 4vmin);
+    margin-right: 5vmin;
+    width: calc(100% - 15.5vmin - 10vmin);
+    height: calc(100% - 21vmin - 8vmin);
+  }
+  .material-design-icon{
+    width: 1.5em;
+    height: 1.5em;
+    top: calc(10.5vmin - .75em + 3.8vmin);
+    right: calc(5vmin - .75em + 5.2vmin);
+    svg{
+      width: 1.5em;
+      height: 1.5em;
+    }
+  }
+
 }
 </style>
--- src/components/SingleGalleryItemModalInner.vue
+++ src/components/SingleGalleryItemModalInner.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="singlegalleryinner">
-    {{image}}
-    <img :src="image.image_file" alt="">
+  <div class="singlegalleryinner" v-on:click.stop="$emit('childclick')">
+    <img :src="image.image_file" alt="" :class="imagefullscreen ? 'imagefullscreen' : null" @click="imagefullscreen = !imagefullscreen">
+    <div v-if="imagefullscreen" class="darkoverlay"></div>
+    <img v-if="imagefullscreen" :src="image.image_file" alt="" :style="{opacity: 0}">
+    {{imagefullscreen}}
+
   </div>
 </template>
 
@@ -14,24 +17,63 @@ export default {
   },
   data(){
     return{
-
+      imagefullscreen: false
     }
   },
   computed: {
 
+  },
+  mounted(){
+    console.dir(this)
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.singlegalleryinner{
+.darkoverlay{
+  position: fixed;
+  left: 0;
+  top: 0;
   width: 100%;
   height: 100%;
-  background: blue;
-  overflow-y: scroll;
+  background: rgba(27,27,27,.9);
+}
+.imagefullscreen{
+  border-radius: 0 !important;
+  z-index: 12345 !important;
+  position: fixed;
+  width: auto !important;
+  height: auto !important;
+  max-width: 100vw !important;
+  max-height: 100vh !important;
+  margin: 0 !important;
+  padding: 0 !important;
+  object-fit: contain;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.singlegalleryinner{
+  position: relative;
+  flex-grow: 1;
+  box-sizing: border-box;
+  padding: 1em;
+  margin-bottom: 12.4vmin;
+  width: 100%;
+  height: auto;
+  //border: 1px solid blue;
+  //background: blue;
   img{
     max-width: 100%;
-    max-height: 100%;
+    max-height: 50vh;
+    overflow: hidden;
+    border-radius: 0 0 $frame-constant*2 0;
+  }
+}
+
+@media (max-width: 800px){
+  .singlegalleryinner{
+    margin-bottom: 14vmin;
   }
 }
 </style>
